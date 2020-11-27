646a22bed1815a3c3dcd7b78610888dd4132c67d
Franco <ffusaro94@gmail.com>
2020-11-27 15:14:28 -0300

    Added cant. de huespedes to publicacion creation

--- app/src/main/java/com/example/bookbnb/viewmodels/NuevaPublicacionViewModel.kt
+++ app/src/main/java/com/example/bookbnb/viewmodels/NuevaPublicacionViewModel.kt
@@ -19,6 +19,7 @@ import com.google.firebase.storage.ktx.storage
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import java.lang.Float.parseFloat
+import java.lang.Integer.parseInt
 
 //TODO: Embed different classes for parts of the viewmodel (i.e. one for info, one for location, etc.)
 class NuevaPublicacionViewModel(application: Application) : BaseAndroidViewModel(application) {
@@ -33,6 +34,10 @@ class NuevaPublicacionViewModel(application: Application) : BaseAndroidViewModel
     private val _price = MutableLiveData<String>("")
     val price: MutableLiveData<String>
         get() = _price
+
+    private val _cantDeHuespedes = MutableLiveData<String>("")
+    val cantDeHuespedes: MutableLiveData<String>
+        get() = _cantDeHuespedes
 
     private val _selectedLocation = MutableLiveData<CustomLocation>()
     val selectedLocation: MutableLiveData<CustomLocation>
@@ -79,7 +84,8 @@ class NuevaPublicacionViewModel(application: Application) : BaseAndroidViewModel
     enum class FormErrors {
         MISSING_TITULO,
         MISSING_DESC,
-        INVALID_PRICE
+        INVALID_PRICE,
+        MISSING_CANT_HUESPEDES
     }
 
     fun setSelectedLocation(location: CustomLocation?) {
@@ -125,6 +131,11 @@ class NuevaPublicacionViewModel(application: Application) : BaseAndroidViewModel
             parseFloat(_price.value!!)
         } catch (e: Exception) {
             formErrors.add(FormErrors.INVALID_PRICE)
+        }
+        try{
+            parseInt(_cantDeHuespedes.value!!)
+        } catch (e: Exception){
+            formErrors.add(FormErrors.MISSING_CANT_HUESPEDES)
         }
         return formErrors.isEmpty()
     }
@@ -210,7 +221,7 @@ class NuevaPublicacionViewModel(application: Application) : BaseAndroidViewModel
                     _desc.value!!,
                     parseFloat(_price.value!!),
                     _selectedLocation.value!!,
-                    2,//TODO: Agregar cantidad de huespedes al form.
+                    parseInt(_cantDeHuespedes.value!!),
                     imagesUrls
                 )
                 when (response) {
